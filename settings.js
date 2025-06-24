8fd1e4cc0d6bddec52296d8260f0072b85ada4e9
DG200710 <buruvawassa@gmail.com>
2025-06-24 18:39:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,27 +1,27 @@
 module.exports = {
     contactsFile: '/contacts.json',
-    imageUrl: 'https://files.catbox.moe/kbkvx3.jpg', 
+    imageUrl: 'https://files.catbox.moe/jtt8o7.jpg', 
     groupLink: 'https://chat.whatsapp.com/K51jDuAWAwdENxOchMzfTS',
     newsletterJid: '120363419505851823@newsletter',
     defaultCaption: 'Welcome to DASSA-TECH! 🚀 Join our community for the latest updates!',
     messageDelay: 1000, 
     dexterTechText: `
 ┏━━━━━━━━━━━━━━
-┃DASSA CONTACT ID
+┃DASSA CONTACT ID 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator = 𖥘⚡ DASSA-TECH ⚡𖥘
+❶ || Creator = 𖥘⚡ PROGAMMER DASSA ⚡𖥘
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || https://whatsapp.com/channel/0029VbAmVQnHwXbJ2e3ROQ1E
+❷ || wp chanel = https://whatsapp.com/channel/0029VbAmVQnHwXbJ2e3ROQ1E
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 ❸ || Owner = https://wa.me/+94743277981
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❺ || web site = https://dassa-profile-web.vercel.app/
+❺ || telegram = https://t.me/dasuntach
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❻ || YouTube = https://youtube.com/@dassatech17?si=1r7W09xIJfGMwlA2
+❻ || YouTube =  https://youtube.com/@dassatech17?si=1r7W09xIJfGMwlA2
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©2024-2099 DASSA-TECH_`,
+©2025_UPDATE PROGAMMER DASSA 💀_`,
     completionMessage: 'Auto messaging to all contacts completed successfully! ✅'
 };
